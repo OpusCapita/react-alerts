fc26153859c0364bd743987bd9e97068db9894ba
Ilkka Lehtinen <ilkka.lehtinen@opuscapita.com>
2017-09-06 11:13:48 +0300

    Fix linting errors in examples

--- examples/index.jsx
+++ examples/index.jsx
@@ -2,31 +2,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
-import { createStore,
-         applyMiddleware,
-         compose,
-         combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import { IntlProvider, intlReducer } from 'react-intl-redux';
-import thunk from 'redux-thunk';
 import AlertsView from './components/alerts-view/alerts-view.component';
-import { alertsReducer, OCAlert } from '../src/index';
 
 import './app.component.scss';
-
-
-const composeEnhancers = (process.env.NODE_ENV !== 'production' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
-/* const store = createStore(
-  combineReducers({
-    alertsReducer,
-    intl: intlReducer,
-  }),
-  composeEnhancers(applyMiddleware(thunk)),
-);
-
-OCAlert.setStore(store); */
 
 render((
   <Router history={hashHistory}>
