f89e38211996049b5154a1336228cfb11c5c0b95
Ilkka Lehtinen <ilkka.lehtinen@opuscapita.com>
2017-09-06 10:06:29 +0300

    Extracted to own repository from oc-common-ui

---
+++ examples/index.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+import { createStore,
+         applyMiddleware,
+         compose,
+         combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { IntlProvider, intlReducer } from 'react-intl-redux';
+import thunk from 'redux-thunk';
+import AlertsView from './components/alerts-view/alerts-view.component';
+import { alertsReducer, OCAlert } from '../src/index';
+
+import './app.component.scss';
+
+require('../images/favicon.ico');
+
+
+const composeEnhancers = (process.env.NODE_ENV !== 'production' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const store = createStore(
+  combineReducers({
+    alertsReducer,
+    intl: intlReducer,
+  }),
+  composeEnhancers(applyMiddleware(thunk)),
+);
+
+OCAlert.setStore(store);
+
+render((
+  <Provider store={store}>
+    <IntlProvider>
+      <Router history={hashHistory}>
+        <Route path="/" component={AlertsView} />
+      </Router>
+    </IntlProvider>
+  </Provider>
+), document.getElementById('oc-examples'));
---
+++ src/alerts/alerts.constants.js
@@ -0,0 +1,5 @@
+const INITIAL_STATE = {
+  alerts: [],
+};
+
+export default INITIAL_STATE;
